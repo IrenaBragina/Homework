337c1af2ccc39baffa54cc7bf64b205fa5683ef1
IrenaBragina <ibelemenko@gmail.com>
2024-08-10 14:26:55 +0500

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,9 @@
+def divide(first,second):
+    if second == 0:
+        return "ошибка"
+
+    return first/second
+
+if __name__ == "__main__":
+    print(divide(50,0))
+
---
+++ true_math.py
@@ -0,0 +1,7 @@
+from math import inf
+def divide(first,second):
+    if second == 0:
+        return inf
+    return first/second
+if __name__ == "__main__":
+    print(divide(50,5))
