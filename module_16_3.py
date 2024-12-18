0fbf067e95cec82221dea715bdf7c3286318602d
IrenaBragina <ibelemenko@gmail.com>
2024-12-18 20:56:31 +0500

    Add files via upload

---
+++ module_16_3.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+users = {'1': 'Имя: Example, возраст: 18'}
+
+@app.get("/users")
+async def get_users()-> dict:
+    return users
+
+@app.post("/user/{username}/{age}")
+async def post_user(message:str,username:str,age:int)->str:
+    user_id = str(int(max(users,key=int))+1)
+    users[user_id]=message
+    return f"User {user_id} is registered"
+
+@app.put("/user/{user_id}/{username}/{age}")
+async def update_user(user_id:str,username:str,age:int)->str:
+    return f"The user {user_id} is updated"
+
+@app.delete("/user/{user_id}")
+async def delete_user(user_id)->str:
+    users.pop(user_id)
+    return f"User {user_id} has been deleted"
